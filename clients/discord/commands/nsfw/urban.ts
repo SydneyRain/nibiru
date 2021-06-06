5607dfba9e62fc79d1a4a478bc40e0a6858ea451
Sydney M <syndeyrain6969@gmail.com>
2021-06-06 00:22:55 -0400

    UD command

---
+++ clients/discord/commands/nsfw/urban.ts
@@ -0,0 +1,55 @@
+/*---------------------------------------------------*/
+import { Description, Discord, Option, Slash } from "@typeit/discord";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+/*---------------------------------------------------*/
+import axios from "axios";
+import { resourceLimits } from "worker_threads";
+
+@Discord()
+export abstract class UrbanCommand {
+    @Slash("urban")
+    @Description('Look up a word on the Urban Dictionary')
+    async urban(
+        @Option("word", {description: "The word to look up", required: true})
+        wordToLookup: string,
+        interaction: CommandInteraction
+    ) {
+        //todo: Add buttons to go through definitions
+        await axios.get(`https://api.urbandictionary.com/v0/define?term=${wordToLookup}`).then(res =>
+        {
+            if (res.status != 200) {
+                return interaction.reply('Error fetching data from Urban Dictionary :( Please try again later!');
+            }
+
+            if (res.data?.list.length <= 0) {
+                return interaction.reply('No results for this word were found...')
+            }
+
+            let wordResult = res.data.list[0];
+
+            interaction.reply({
+                embeds: [
+                    new MessageEmbed({
+                        author: {
+                            name: 'Urban Dictionary',
+                            iconURL: 'https://i.ibb.co/PZsnHZ6/ud.jpg',
+                        },
+                        title: wordResult.word,
+                        description: wordResult.definition,
+                        fields: [
+                            {
+                                name: 'Example',
+                                value: wordResult.example
+                            },
+                            {
+                                name: 'Score',
+                                value: `${wordResult.thumbs_up - wordResult.thumbs_down} (${wordResult.thumbs_up} 👍 ${wordResult.thumbs_down} 👎)`
+                            }
+                        ],
+                        footer: { text: `By ${wordResult.author} - ${wordResult.written_on.replace(/T/, ' ').replace(/\..+/, '')}` }
+                    }),
+                ],
+            });
+        });
+    }
+}
--- clients/discord/guards/IsBotOwner.ts
+++ clients/discord/guards/IsBotOwner.ts
@@ -1,13 +1,19 @@
+/*---------------------------------------------------*/
 import { GuardFunction } from "@typeit/discord";
 import { CommandInteraction } from "discord.js";
+/*---------------------------------------------------*/
+import * as fs from 'fs';
+import * as yaml from "js-yaml";
+/*---------------------------------------------------*/
+const config = yaml.load(fs.readFileSync(__dirname + '/../../../config.yml', 'utf8'));
+/*---------------------------------------------------*/
 
 export const IsBotOwner: GuardFunction<CommandInteraction> = async (
   interaction,
   client,
   next
 ) => {
-
-    if(interaction.user.id !== '145788286156144641') {
+    if(interaction.user.id !== config.botConfig.discordConfig.botOwner) {
         return interaction.reply('You are not allowed to use this command.');
     }
     else {
